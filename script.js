4da0f1dab0e395b0b65b12e65967a83a32e66493
lutvaliyev.r <lutvaliyev.r@gmail.com>
2022-02-07 23:03:58 +0400

    design changed

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-$.attractHover('.logo img',{
-    proximity: 30,
-    magnetism: 4
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.setAttribute("style", "top: "+ (e.pageY-150) +"px; left:"+(e.pageX-450) + "px;");
 });
 
+// $('.social-links').hover(function(){
+//     // $('.link').css('border-color', 'red');
+//     // $(this).find('i').css('display', 'none');
+// });
