6c2db1cae7a735d8591a184e902b7fb2a3bab4b8
lutvaliyev.r <lutvaliyev.r@gmail.com>
2022-02-09 00:19:38 +0400

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', (e) => {
-    cursor.setAttribute("style", "top: "+ (e.pageY-150) +"px; left:"+(e.pageX-400) + "px;");
+    cursor.setAttribute("style", "top: "+ (e.pageY-75) +"px; left:"+(e.pageX-320) + "px;");
 });
 
 // $('.social-links').hover(function(){
