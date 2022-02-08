d9e5d88d2cebf990fda47f59515c5baa57b7e3d8
lutvaliyev.r <lutvaliyev.r@gmail.com>
2022-02-08 09:44:40 +0400

    mobile bugs fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove', (e) => {
-    cursor.setAttribute("style", "top: "+ (e.pageY-150) +"px; left:"+(e.pageX-450) + "px;");
+    cursor.setAttribute("style", "top: "+ (e.pageY-150) +"px; left:"+(e.pageX-400) + "px;");
 });
 
 // $('.social-links').hover(function(){
